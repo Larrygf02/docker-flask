ca56f2421cf9fe3e7dba8ff0909df9e0a788531b
larrygf02 <raulgf_02@hotmail.com>
2020-04-09 19:28:31 -0500

    Render template in flask

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return 'Hola mundo'
+
+@app.route("/home")
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
