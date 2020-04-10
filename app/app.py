112bf692cdd9058302aa58d4d345d37ba09b3902
larrygf02 <raulgf_02@hotmail.com>
2020-04-09 23:04:30 -0500

    Connecting to DB Postgres

--- app/app.py
+++ app/app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, requestm jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 ## Config DB
 DB_URL = 'postgresql+psycopg2://postgres:123@db/crudperson'
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 ## Class
 class Person(db.Model):
     __tablename__ = 'persons'
-    id = db.Column(Integer, primary_key=True, server_default=text("nextval('persons_id_seq'::regclass)"))
+    id = db.Column(db.Integer, primary_key=True, server_default=db.text("nextval('persons_id_seq'::regclass)"))
     name = db.Column(db.String(50))
     edad = db.Column(db.Integer)
 
@@ -22,7 +25,7 @@ def index():
 def home():
     return render_template('index.html')
 
-@app.route("/person", methods["POST"])
+@app.route("/person", methods=["POST"])
 def postPerson():
     parametros = request.get_json()
     new_person = Person(**parametros)
