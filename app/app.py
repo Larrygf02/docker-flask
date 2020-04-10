9469a2912ad34a00d07d3033f1ed0583a22a8649
larrygf02 <raulgf_02@hotmail.com>
2020-04-09 19:17:14 -0500

    Config run local

--- app/app.py
+++ app/app.py
@@ -6,4 +6,4 @@ def index():
     return 'Hola mundo'
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=4000)
+    app.run(debug=True,host='0.0.0.0')
--- deploy/staging.sh
+++ deploy/staging.sh
@@ -3,4 +3,4 @@ test ! -z "$(docker ps -aqf name=c-flaskd-stg)" && docker rm -f c-flaskd-stg && 
 test ! -z "$(docker images -q i-flaskd-stg)" && docker rmi i-flaskd-stg && echo "image staging removed"
 
 docker build -t i-flaskd-stg .
-docker run -d -p 5000:4000 --name c-flaskd-stg i-flaskd-stg
+docker run -d -e PORT=5000 -p 5000:5000 --name c-flaskd-stg i-flaskd-stg
