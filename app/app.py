4f92841a507b4543ec9ef29690bfd836674f9621
larrygf02 <raulgf_02@hotmail.com>
2020-04-19 11:59:06 -0500

    Docker flask complete

--- app/app.py
+++ app/app.py
@@ -3,11 +3,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Hola mundo'
+    return 'Hola mundo desde aqui'
 
 @app.route("/home")
 def home():
     return render_template('index.html')
+
+@app.route("/hello")
+def hello():
+    return 'Hello app'
+
+@app.route("/say")
+def say():
+    name = 'Grace'
+    return f'Hi {name}'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=5000)
